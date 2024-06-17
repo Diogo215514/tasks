0852a0c684983bd566724830784a0dd578f5718f
Diogo215514 <marcosdiogovj@gmail.com>
2024-06-17 08:42:14 -0300

    Alteração task_list e task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,22 @@
 import react from "react"
-import {Text, View, StyleSheet} from "react-native"
+import {Text, View, StyleSheet, TouchableWithoutFeedback} from "react-native"
 import moment from "moment"
+import { Icon } from "react-native-vector-icons/Icon/FontAwesoume6"
 
 export default props => {
     return(
         <View style={styles.container}>
+            <TouchableWithoutFeedback>
+                <view style={styles.checkContainer}>
+                    <view style={styles.done}>
+                        <Icon name="check" size={20} color="#FFF"/>
+                    </view>
+                </view>
+            </TouchableWithoutFeedback>
             <View>
-                <Text>{props.description}</Text>
-                <Text>{props.estimate_at}</Text>
-                <Text>{props.done_at}</Text>
+                <Text style={styles.desc}>{props.description}</Text>
+                <Text style={styles.date}>{props.estimate_at}</Text>
+                <Text style={styles.date}>{props.done_at}</Text>
             </View>
         </View>
     )
@@ -19,6 +27,36 @@ const styles = StyleSheet.create({
         borderColor: '#AAA',
         borderBottomWidth: 1,
         alignItems: 'center',
-        paddingVertical: 10
+        paddingVertical: 10,
+    },
+    checkContainer:{
+        width: "20%",
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+    done:{
+        height: 25,
+        width: 20,
+        borderRadius: 13,
+        borderWidth: 1,
+        backgroundColor: "#4D7031",
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+    pending:{
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: "#555"
+    },
+    desc: {
+        fontFamily: "Arial",
+        fontSize: 20,
+        color: "333"
+    },
+    date: {
+        fontFamily: "Arial",
+        color: "333"
     }
 })
